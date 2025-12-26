4cafa251ce763b1fcd805f15f21c8d41f39c6a71
Satyam Pandey <satyam.pandey@tietoevry.com>
2025-12-26 10:43:29 +0530

    Initial project setup with Gradle

---
+++ src/main/java/com/surest/membermanagementassignment/MemberManagementAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.surest.membermanagementassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagementAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberManagementAssignmentApplication.class, args);
+    }
+
+}

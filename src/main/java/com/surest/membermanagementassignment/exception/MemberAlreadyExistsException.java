a4d3893e1eec011316c10813edbfd9fdddaabc51
Satyam Pandey <satyam.pandey@tietoevry.com>
2025-12-26 14:02:01 +0530

    Added exception handling

---
+++ src/main/java/com/surest/membermanagementassignment/exception/MemberAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.surest.membermanagementassignment.exception;
+
+public class MemberAlreadyExistsException extends RuntimeException {
+
+    public MemberAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
